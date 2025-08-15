db8a56d942664010793ec4c01a0e00420fd308e2
Alvin Zhao <alvin@flip.com.au>
2025-08-15 12:42:36 +1000

    package: dump

--- config/packages.sh
+++ config/packages.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
+sudo apt install git
+sudo apt install tmux
 sudo apt install screenfetch
+sudo apt install fzf
+
+# For emacs.. extract it?
+sudo apt install ripgrep
+sudo apt install fd-find
