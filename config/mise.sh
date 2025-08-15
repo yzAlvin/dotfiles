22a789073bbcfb3afed31f605e78fc5fbd245831
Alvin Zhao <alvin@flip.com.au>
2025-08-15 17:45:04 +1000

    config: Move some stuff from my .bashrc into env

--- config/mise.sh
+++ config/mise.sh
@@ -4,4 +4,4 @@
 
 curl https://mise.run | sh
 
-echo 'eval "$(mise activate bash)"' >> ~/.bashrc
+# echo 'eval "$(mise activate bash)"' >> ~/.bashrc
--- config/starship.sh
+++ config/starship.sh
@@ -4,4 +4,4 @@
 
 curl -sS https://starship.rs/install.sh | sh
 
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
+# echo 'eval "$(starship init bash)"' >> ~/.bashrc
